20a4649d81a22132db274d6cb3e6798b7537d681
xdrie <xdrie@alt.icu>
2020-12-28 14:52:06 -0800

    make lifetime update reliable

--- src/Glint.Networking/Messages/Body/BodyLifetimeUpdateMessage.cs
+++ src/Glint.Networking/Messages/Body/BodyLifetimeUpdateMessage.cs
@@ -1,7 +1,10 @@
+using Lidgren.Network;
 using MsgPack.Serialization;
 
 namespace Glint.Networking.Messages {
     public class BodyLifetimeUpdateMessage : BodyUpdateMessage {
         [MessagePackMember(4)] public bool exists { get; set; }
+
+        public override NetDeliveryMethod deliveryMethod { get; } = NetDeliveryMethod.ReliableOrdered;
     }
 }
