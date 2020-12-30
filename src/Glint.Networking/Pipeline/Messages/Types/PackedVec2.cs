af0485eb56b2da8d39e16ef3ad2c0437feba09df
xdrie <xdrie@alt.icu>
2020-12-29 22:47:00 -0800

    back to class :(

--- src/Glint.Networking/Pipeline/Messages/Types/PackedVec2.cs
+++ src/Glint.Networking/Pipeline/Messages/Types/PackedVec2.cs
@@ -2,10 +2,12 @@ using Microsoft.Xna.Framework;
 using MsgPack.Serialization;
 
 namespace Glint.Networking.Messages.Types {
-    public struct PackedVec2 {
+    public class PackedVec2 {
         [MessagePackMember(0)] public float x { get; set; }
         [MessagePackMember(1)] public float y { get; set; }
 
+        public PackedVec2() { }
+        
         public PackedVec2(Vector2 v) : this(v.X, v.Y) { }
 
         public PackedVec2(float x, float y) {
