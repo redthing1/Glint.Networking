62c3e3ae20590c7ead96e41009a59beb6003fe01
xdrie <xdrie@alt.icu>
2020-12-29 18:05:11 -0800

    fix registration types

--- src/Glint.Networking/Pipeline/Handlers/BodyUpdateHandler.cs
+++ src/Glint.Networking/Pipeline/Handlers/BodyUpdateHandler.cs
@@ -3,26 +3,15 @@ using Glint.Networking.Game.Updates;
 using Glint.Networking.Messages;
 
 namespace Glint.Networking.Pipeline.Handlers {
-    public abstract class BodyUpdateHandler : ClientMessageHandler<BodyUpdateMessage> {
+    public abstract class BodyUpdateHandler<TMessage> : ClientMessageHandler<TMessage> where TMessage : BodyUpdateMessage {
         public BodyUpdateHandler(GameSyncer syncer) : base(syncer) { }
+    }
 
-        public override bool handle(BodyUpdateMessage msg) {
+    public class BodyKinematicUpdateHandler : BodyUpdateHandler<BodyKinematicUpdateMessage> {
+        public BodyKinematicUpdateHandler(GameSyncer syncer) : base(syncer) { }
+        public override bool handle(BodyKinematicUpdateMessage msg) {
             // we always clone the message because we are pooling instances
-            var update = default(BodyUpdate);
-            // TODO: instance pooling of body updates
-            switch (msg) {
-                case BodyKinematicUpdateMessage kinMsg:
-                    update = new BodyKinUpdate();
-                    break;
-                case BodyLifetimeUpdateMessage lifMsg:
-                    update = new BodyLifetimeUpdate();
-                    break;
-                default:
-                    Global.log.err(
-                        $"failed to create message queue clone of unmatched message type {msg.GetType()}");
-                    return false; // unable to handle (unrecognized)
-            }
-
+            var update = new BodyKinUpdate();
             update.copyFrom(msg);
 
             syncer.bodyUpdates.enqueue(update);
@@ -30,11 +19,15 @@ namespace Glint.Networking.Pipeline.Handlers {
         }
     }
 
-    public class BodyKinematicUpdateHandler : BodyUpdateHandler {
-        public BodyKinematicUpdateHandler(GameSyncer syncer) : base(syncer) { }
-    }
-
-    public class BodyLifetimeUpdateHandler : BodyUpdateHandler {
+    public class BodyLifetimeUpdateHandler : BodyUpdateHandler<BodyLifetimeUpdateMessage> {
         public BodyLifetimeUpdateHandler(GameSyncer syncer) : base(syncer) { }
+        public override bool handle(BodyLifetimeUpdateMessage msg) {
+            // we always clone the message because we are pooling instances
+            var update = new BodyLifetimeUpdate();
+            update.copyFrom(msg);
+
+            syncer.bodyUpdates.enqueue(update);
+            return true;
+        }
     }
 }
--- src/Glint.Networking/Pipeline/Relays/BodyUpdateRelay.cs
+++ src/Glint.Networking/Pipeline/Relays/BodyUpdateRelay.cs
@@ -1,20 +1,20 @@
 using Glint.Networking.Messages;
 
 namespace Glint.Networking.Pipeline.Relays {
-    public abstract class BodyUpdateRelay : ServerMessageRelay<BodyUpdateMessage> {
+    public abstract class BodyUpdateRelay<TMessage> : ServerMessageRelay<TMessage> where TMessage : BodyUpdateMessage {
         public BodyUpdateRelay(GlintNetServerContext context) : base(context) { }
 
-        protected override bool validate(BodyUpdateMessage msg) {
-            // TODO: proper validation logic for body updates
+        protected override bool validate(TMessage msg) {
+            // validation logic for body updates?
             return true;
         }
     }
 
-    public class BodyKinematicUpdateRelay : BodyUpdateRelay {
+    public class BodyKinematicUpdateRelay : BodyUpdateRelay<BodyKinematicUpdateMessage> {
         public BodyKinematicUpdateRelay(GlintNetServerContext context) : base(context) { }
     }
 
-    public class BodyLifetimeUpdateRelay : BodyUpdateRelay {
+    public class BodyLifetimeUpdateRelay : BodyUpdateRelay<BodyLifetimeUpdateMessage> {
         public BodyLifetimeUpdateRelay(GlintNetServerContext context) : base(context) { }
     }
 }
