3c3977fe4bc9648fc1f5949d8d9da6563ea48bb2
xdrie <xdrie@alt.icu>
2020-12-30 19:28:29 -0800

    netNode -> node

--- src/Glint.Networking/Game/ClientGameSyncer.cs
+++ src/Glint.Networking/Game/ClientGameSyncer.cs
@@ -7,23 +7,23 @@ namespace Glint.Networking.Game {
         public ClientGameSyncer(LimeClient node, string host, int port, string nickname, int netUps, int systemUps, int ringBufferSize, bool debug = false) :
             base(node, host, port, nickname, netUps, systemUps, ringBufferSize, debug) { }
 
-        public new LimeClient netNode => (LimeClient) base.netNode;
+        public new LimeClient node => (LimeClient) base.netNode;
 
         public void connect() {
             // open connection and go
             Global.log.info($"connecting to server node ({host}:{port})");
-            netNode.start();
-            netNode.connect(host, port, "hail");
-            nodeUpdateTimer = Core.Schedule(1f / netUps, true, timer => { netNode.update(); });
+            node.start();
+            node.connect(host, port, "hail");
+            nodeUpdateTimer = Core.Schedule(1f / netUps, true, timer => { node.update(); });
         }
 
         public void disconnect() {
             Global.log.info($"requesting disconnect from server");
-            var intro = netNode.getMessage<PresenceMessage>();
-            intro.myUid = netNode.uid;
+            var intro = node.getMessage<PresenceMessage>();
+            intro.myUid = node.uid;
             intro.here = false;
-            netNode.sendToAll(intro);
-            netNode.disconnect();
+            node.sendToAll(intro);
+            node.disconnect();
         }
     }
 }
