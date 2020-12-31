a56980b99492c0643195c238404e56b862a36ff0
xdrie <xdrie@alt.icu>
2020-12-30 19:30:34 -0800

    add nullable annotation

--- src/Lime/LimeClient.cs
+++ src/Lime/LimeClient.cs
@@ -3,7 +3,7 @@ using Lidgren.Network;
 namespace Lime {
     public class LimeClient : LimeNode {
         public NetClient lidgrenClient => (NetClient) lidgrenPeer;
-        public NetConnection serverConn => lidgrenClient.ServerConnection;
+        public NetConnection? serverConn => lidgrenClient.ServerConnection;
         public LimeClient(Configuration config) : base(config, new NetClient(config.peerConfig)) { }
 
         public NetConnection connect(string host, in int port, string hail) {
